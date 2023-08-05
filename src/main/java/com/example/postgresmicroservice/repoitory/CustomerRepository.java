a2c381ee52a404f0893b8b18fe0a2bef0b53b037
fuadmuradov-em <fuad.muradov@emanat.az>
2023-08-05 15:46:48 +0400

    update repository

--- src/main/java/com/example/postgresmicroservice/repoitory/CustomerRepository.java
+++ src/main/java/com/example/postgresmicroservice/repoitory/CustomerRepository.java
@@ -3,6 +3,9 @@ package com.example.postgresmicroservice.repoitory;
 import com.example.postgresmicroservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByName(String name);
 }
 
--- src/main/java/com/example/postgresmicroservice/repoitory/TransactionRepository.java
+++ src/main/java/com/example/postgresmicroservice/repoitory/TransactionRepository.java
@@ -1,8 +1,13 @@
 package com.example.postgresmicroservice.repoitory;
 
 import com.example.postgresmicroservice.entity.Transaction;
+import com.example.postgresmicroservice.enums.TransactionTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
 }
